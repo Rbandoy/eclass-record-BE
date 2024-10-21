59beef94a6fa47ba8e94ed0fd08e6f5ee10b6d62
Rene Boy Andoy <reneboyandoy@Renes-MacBook-Air.local>
2024-10-21 21:45:02 +0800

    updated

--- src/api/grade-masterlist/controllers/grade-masterlist.js
+++ src/api/grade-masterlist/controllers/grade-masterlist.js
@@ -13,7 +13,7 @@ module.exports = createCoreController('api::grade-masterlist.grade-masterlist', 
   async create(ctx) {
     const sendSms = async (mobileNo, message) => {
       try { 
-        axios.post(`https://api.semaphore.co/api/v4/messages?apikey=8e9c48fc63c1e60af9c024905e22ecea&number=${mobileNo}&message=${message}&sendername`);
+        axios.post(`https://api.semaphore.co/api/v4/messages?apikey=616d08668be5f09dead2b65f8ff943de&number=${mobileNo}&message=${message}&sendername`);
         console.log("message sent")
       } catch (error) {
         console.log("error sending sms", error)
